5049f9272d4d61c59c9bd8d25481b368e0d83eee
Alexandru Leasenco <rubydeve@gmail.com>
2022-11-06 22:08:29 +0200

    Autoload ActivityCategory and ActivityStatus

--- lib/creatio.rb
+++ lib/creatio.rb
@@ -1,22 +1,26 @@
 # frozen_string_literal: true
 
-require "faraday"
-require "creatio/version"
+require 'faraday'
+require 'creatio/version'
 
 module Creatio
-  autoload :Client, "creatio/client"
-  autoload :OAuth2, "creatio/oauth2"
-  autoload :Error, "creatio/error"
-  autoload :Object, "creatio/object"
-  autoload :Resource, "creatio/resource"
+  autoload :Client, 'creatio/client'
+  autoload :OAuth2, 'creatio/oauth2'
+  autoload :Error, 'creatio/error'
+  autoload :Object, 'creatio/object'
+  autoload :Resource, 'creatio/resource'
 
-  autoload :Activity, "creatio/objects/activity"
-  autoload :Contact, "creatio/objects/contact"
-  autoload :Deal, "creatio/objects/deal"
-  autoload :Lead, "creatio/objects/lead"
+  autoload :Activity, 'creatio/objects/activity'
+  autoload :ActivityCategory, 'creatio/objects/activity_category'
+  autoload :ActivityStatus, 'creatio/objects/activity_status'
+  autoload :Contact, 'creatio/objects/contact'
+  autoload :Deal, 'creatio/objects/deal'
+  autoload :Lead, 'creatio/objects/lead'
 
-  autoload :ActivityResource, "creatio/resources/activities"
-  autoload :ContactResource, "creatio/resources/contacts"
-  autoload :DealResource, "creatio/resources/deals"
-  autoload :LeadResource, "creatio/resources/leads"
+  autoload :ActivityResource, 'creatio/resources/activities'
+  autoload :ActivityCategory, 'creatio/resources/activity_categories'
+  autoload :ActivityStatus, 'creatio/resources/activity_statuses'
+  autoload :ContactResource, 'creatio/resources/contacts'
+  autoload :DealResource, 'creatio/resources/deals'
+  autoload :LeadResource, 'creatio/resources/leads'
 end
